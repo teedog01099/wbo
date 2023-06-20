ac305f9f5d51cca5bc2c5bf758f34fde098f39f5
teedog01099 <timfuchs@lithec.de>
2023-06-20 20:18:27 +0200

    automated commit

--- server/client_configuration.js
+++ server/client_configuration.js
@@ -3,6 +3,7 @@ const config = require("./configuration");
 /** Settings that should be handed through to the clients  */
 module.exports = {
   ENV: config.ENV,
+  STEM: config.STEM,
   MAX_BOARD_SIZE: config.MAX_BOARD_SIZE,
   MAX_EMIT_COUNT: config.MAX_EMIT_COUNT,
   MAX_EMIT_COUNT_PERIOD: config.MAX_EMIT_COUNT_PERIOD,
